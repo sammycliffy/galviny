bfe7469088daa65a66bca7e47a7a749d09f10132
Sammy_cliffy <sammycliffy15@gmail.com>
2018-06-10 21:02:40 +0000

    init

--- galviny_project/settings.py
+++ galviny_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!4q4810=m97-3icmpv7_@=ua-)v0xfgh_06g(nk7!yniaeaors'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['galviny2.herokuapp.com']
 
 
 # Application definition
