9bc661f635518e9fd67063aa1da3ab8e8680c0fc
Sammy_cliffy <sammycliffy15@gmail.com>
2018-06-15 15:44:14 +0000

    init

--- galviny_project/settings.py
+++ galviny_project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['galviny2.herokuapp.com', 'localhost']
 
 INSTALLED_APPS = [
     'app',
-    'celery',
     'django_celery_beat',
     'background_task',
     'django.contrib.admin',
