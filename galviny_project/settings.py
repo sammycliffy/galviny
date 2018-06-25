413a80372c9339a46e90864eb31a2d272f3f8bd5
Sammy_cliffy <sammycliffy15@gmail.com>
2018-06-25 13:57:26 +0100

    init

--- galviny_project/settings.py
+++ galviny_project/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'galviny_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+
+
+
+
 
 
 # Password validation
