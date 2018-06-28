ed7729b1d30a0156d81a822b1ca79e278d09fcd4
Sammy_cliffy <sammycliffy15@gmail.com>
2018-06-28 07:29:01 +0100

    init

--- galviny_project/settings.py
+++ galviny_project/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'galviny_project.wsgi.application'
   #      'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
-}
+#}
 
 DATABASES = {
   'default': dj_database_url.config()
