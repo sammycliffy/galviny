5010e495196d6ac625103b8b5d7969fef6449302
Sammy_cliffy <sammycliffy15@gmail.com>
2018-08-29 11:25:14 +0100

    init

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -79,7 +79,7 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Payable_referral',
+            name='Payable_referred',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('username', models.CharField(max_length=255, null=True)),
